fe4f0e58704b273b95fff88aea7b2ceb9ade3df3
nabila <nabilatajrin@gmail.com>
2019-07-29 14:15:59 +0600

    2:16PM 29JUL2019 move multiple files project created

---
+++ move_multiple_files/Main.py
@@ -0,0 +1,21 @@
+import os
+
+# Function to move files
+def main():
+    i = 1
+
+    for filename in os.listdir("/media/iit/Workspace/test/multipleFiles/srcFolder"):
+        #to rename file
+        #file = str(i) + ".ppm.bz2"
+        src = '/media/iit/Workspace/test/multipleFiles/srcFolder/' + filename
+        dst = '/media/iit/Workspace/test/multipleFiles/destFolder/' + filename
+
+        print(filename)
+        # move all the files
+        os.rename(src, dst)
+        #i += 1
+
+# Driver Code
+if __name__ == '__main__':
+    # Calling main() function
+    main()
