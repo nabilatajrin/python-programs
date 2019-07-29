960d09494fac5612bdf0277f678367351d524c8e
nabila <nabilatajrin@gmail.com>
2019-07-29 15:23:49 +0600

    3:23PM 29JUL2019 move files from multiple sources project created

--- move_multiple_files_from_different_source/Main.py
+++ move_multiple_files_from_different_source/Main.py
@@ -2,16 +2,22 @@ import os
 
 # Function to move files
 def main():
-    i = 1
 
-    for filename in os.listdir("/media/iit/Workspace/test/multipleFiles/srcFolder"):
-        src = '/media/iit/Workspace/test/multipleFiles/srcFolder/' + filename
-        dst = '/media/iit/Workspace/test/multipleFiles/destFolder/' + filename
-
+    for filename in os.listdir("/media/iit/Workspace/test/multipleSrc"):
         print(filename)
-        # move all the files
-        os.rename(src, dst)
-        #i += 1
+
+        i = 1
+        for filename2 in os.listdir("/media/iit/Workspace/test/multipleSrc/" + filename):
+            print(filename2)
+
+           #file = str(i) + ".ppm.bz2"
+            src = '/media/iit/Workspace/test/multipleSrc/' + filename + '/' + filename2
+            dst = '/media/iit/Workspace/test/multipleSrc/dstFolder/' + filename2
+
+            # move all the files
+            os.rename(src, dst)
+
+            i += 1
 
 # Driver Code
 if __name__ == '__main__':
