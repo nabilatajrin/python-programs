e0968bc8c9df3a9d89cc91effe04934fcd74424d
nabila <nabilatajrin@gmail.com>
2019-07-29 13:17:51 +0600

    1:17PM 29JUL2019 move file from external directory project created

---
+++ move_external_file/Main.py
@@ -0,0 +1,13 @@
+import os
+
+# Function to move files
+def main():
+    prevName = '/media/iit/Workspace/test/3.ppm.bz2'
+    newName = '/media/iit/Workspace/test/destFolder/test/5.ppm.bz2'
+
+    os.rename(prevName, newName)
+
+# Driver Code
+if __name__ == '__main__':
+    # Calling main() function
+    main()
