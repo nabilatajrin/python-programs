6a55c678fa417cfe0619fe609272b9431aee7f3a
nabila <nabilatajrin@gmail.com>
2019-07-31 16:31:40 +0600

    4:31PM 31JUL2019 image_to_image project is created

--- blurring_an_image/Main.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image
-
-image1 = Image.open('test.ppm')
-image2 = Image.open('treepic.jpg')
-image1.show()
-image2.show()
-image1.save('test.png')
-
-
--- image_manipulation/Test.py
+++ image_manipulation/Test.py
@@ -1,5 +1,3 @@
 size_300 = (300, 300)
 
 #resize the image to 300*300 and save into 300 folder
-        image1.thumbnile(size_300)
-        image1.save('300/{}_300{}'.format(fn, fext))
