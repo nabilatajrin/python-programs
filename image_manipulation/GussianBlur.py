f297a37491f70535a7d752f876e835f01c735497
nabila <nabilatajrin@gmail.com>
2019-07-31 13:14:53 +0600

    1:14PM 31JUL2019 image manipulation project updated

---
+++ image_manipulation/GussianBlur.py
@@ -0,0 +1,16 @@
+from PIL import Image, ImageFilter
+import os
+
+srcPath = "/media/iit/R a i n/2019/ML/pythonprojects/image_manipulation/pngs/"
+dstPath = "/media/iit/R a i n/2019/ML/pythonprojects/image_manipulation/blurred_image/"
+dirs = os.listdir(srcPath)
+
+def gaussianBlur():
+    for item in dirs:
+        if os.path.isfile(srcPath + item):
+            im = Image.open(srcPath + item)
+            file, e = os.path.splitext(dstPath + item)
+            imblur = im.filter(ImageFilter.GaussianBlur(15))
+            imblur.save(file + '.jpg', 'JPEG', quality = 90)
+
+gaussianBlur()
