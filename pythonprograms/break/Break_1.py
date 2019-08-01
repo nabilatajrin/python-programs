224a9fc1b488cca2623a71c32634a9f36577162f
nabila <nabilatajrin@gmail.com>
2019-08-01 16:13:02 +0600

    4:12PM 01AUG2019 py program updated

--- pythonprograms/break/Break_1.py
+++ pythonprograms/break/Break_1.py
@@ -5,6 +5,7 @@ found = "no"
 for fruit in fruits:
     if fruit == "jambura":
         found = "yes"
+        print("found it!")
         break
 
 if found == "yes":
--- pythonprograms/continue/Continue.py
+++ pythonprograms/continue/Continue.py
@@ -1,7 +1,12 @@
 sum = 0
-for i in range(1, 100):
+for i in range(1, 10):
     if i % 2 == 0:
         continue
-    sum = sum + i
+    print("i = ", i)
 
-print (sum)
+    sum = sum + i
+    print("sum = ", sum)
+
+print(sum)
+
+print(1+3+5+7+9)
