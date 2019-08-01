5057c080193f17a0c6ed98ca55ce81836379cfbf
nabila <nabilatajrin@gmail.com>
2019-08-01 13:33:58 +0600

    1:33PM 01AUG2019 image_to_image: boxBlur pproject created

--- image_to_image/BoxBlurMerge.py
+++ image_to_image/BoxBlurMerge.py
@@ -1,8 +1,9 @@
+import PIL
 from PIL import Image, ImageFilter
 import os
 
 srcPath = '/media/iit/R a i n/2019/Intelligent Machines Ltd/face-propagation/dummy-data-set/colorferet/colorferet/test_blur/src_blur/'
-dstPath = '/media/iit/R a i n/2019/Intelligent Machines Ltd/face-propagation/dummy-data-set/colorferet/colorferet/test_blur/gaussian_blur/'
+dstPath = '/media/iit/R a i n/2019/Intelligent Machines Ltd/face-propagation/dummy-data-set/colorferet/colorferet/test_blur/box_blur/'
 dirs = os.listdir(srcPath)
 
 #convert images to .jpg and save in a different directory
@@ -14,10 +15,7 @@ def image_to_image():
             #img.show(item)
 
             file, e = os.path.splitext(dstPath + item)
-            img_blur = img.filter(ImageFilter.GaussianBlur(10))
-            # multiply each pixel by 2.2, (e.g. image contrast manipulation)
-            #out = img.point(lambda i: i * 2.2)
-            #img_blur.show(item)
+            img_blur = img.filter(PIL.ImageFilter.BoxBlur(10))
             img_Resize = img.resize((256, 256), Image.ANTIALIAS)
             blur_img_resize = img_blur.resize((256, 256), Image.ANTIALIAS)
             img_Resize.save(file + '.jpg', 'JPEG', quality = 90)
@@ -25,8 +23,8 @@ def image_to_image():
 
 image_to_image()
 
-dstPath = "/media/iit/R a i n/2019/Intelligent Machines Ltd/face-propagation/dummy-data-set/colorferet/colorferet/test_blur/gaussian_blur/"
-mrgPath = "/media/iit/R a i n/2019/Intelligent Machines Ltd/face-propagation/dummy-data-set/colorferet/colorferet/test_blur/original_and_gaussian_mrg/"
+dstPath = "/media/iit/R a i n/2019/Intelligent Machines Ltd/face-propagation/dummy-data-set/colorferet/colorferet/test_blur/box_blur/"
+mrgPath = "/media/iit/R a i n/2019/Intelligent Machines Ltd/face-propagation/dummy-data-set/colorferet/colorferet/test_blur/original_and_box_blur_mrg/"
 dirs2 = os.listdir(dstPath)
 images = list(map(Image.open, [(dstPath + i) for i in dirs2]))
 
@@ -47,7 +45,7 @@ def mergeImage():
         if (i % 2 == 0):
             j += 1
             x_offset = 0
-            new_im.save(mrgPath + "gaussian_blur" + str(j) + '.jpg')
+            new_im.save(mrgPath + "box_blur" + str(j) + '.jpg')
             #new_im.show()
 
 mergeImage()
