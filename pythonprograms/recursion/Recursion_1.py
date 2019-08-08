13b3c94a0ffb015f5415833309778832f63d7f09
nabila <nabilatajrin@gmail.com>
2019-08-08 12:38:57 +0600

    12:38PM 08AUG2019 py program updated

--- pythonprograms/recursion/Recursion_1.py
+++ pythonprograms/recursion/Recursion_1.py
@@ -3,6 +3,7 @@ def factorial(x):
         return 1
     else:
         return x * factorial(x - 1)
+        #return factorial(x), this doesn't work
 
 print (factorial(5))
-print(5*4*3*2*1)
+#print(5*4*3*2*1)
