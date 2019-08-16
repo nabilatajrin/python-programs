74352a9a57ddab5ffcd4f5814695f493f9c35d04
nabila <nabilatajrin@gmail.com>
2019-08-16 11:38:18 +0600

    11:38PM 16AUG2019 py program updated

--- pythonprograms/test.py
+++ pythonprograms/test.py
@@ -1,1 +1,17 @@
-print (2**38)
+fruits = ["apple", "orange", "jambura", "banana", "mango", "cherry"]
+
+#print(fruits[0+1])
+
+found = "no"
+
+for fruits in fruits:
+    if fruits == "jambura":
+        found = "yes"
+        print("found it!")
+        break
+
+if found == "yes":
+    print("we have jambura")
+else:
+    print("sorry")
+
