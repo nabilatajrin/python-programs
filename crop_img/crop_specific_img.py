3181563f1f305424996231b39371380329094dca
nabila <nabilatajrin@gmail.com>
2019-08-26 14:54:35 +0600

    2:54PM 26AUG2019 py program updated

--- crop_img/crop_specific_img.py
+++ crop_img/crop_specific_img.py
@@ -4,14 +4,15 @@ from PIL import Image
 def main():
     try:
         # Relative Path
-        img = Image.open("Laabher_Bazar_07-08-2019_1559_0010.jpg")
+        img = Image.open("Laabher_Bazar_07-08-2019_1601_0001.jpg")
         width, height = img.size
 
-        area = (0, 0, width / 2, height / 2)
+        #area = (0, 0, width / 2, height / 2)
+        area = (60.4607142857, 335.6815286624, 144.5096938776, 397.0079617834)
         img = img.crop(area)
 
         # Saved in the same relative location
-        img.save("cropped_picture.jpg")
+        img.save("cropped_picture1.jpg")
 
     except IOError:
         pass
