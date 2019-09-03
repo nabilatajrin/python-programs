c90204c0a23d5cbf3c62da31f0c5c31108957215
nabila <nabilatajrin@gmail.com>
2019-09-03 14:33:53 +0600

    2:33PM 03SEP2019 py program updated

--- pythonprograms/escape_characters/print_double_quote.py
+++ pythonprograms/escape_characters/print_double_quote.py
@@ -1,1 +1,9 @@
+#\' for single quote, \" for double quote
 print ("I am 5'9\" tall")
+print ('I am 5\'9” tall')
+print ("I am 6' tall")
+
+#If you do not want characters preceded by the \ character to be
+#interpreted as special characters, you can use raw strings by adding an r
+#before the first quote.
+print (r'Hello\tWorld')
