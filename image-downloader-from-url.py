22481ff755a77bbb7be821ee33c5add2b41d53d9
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2020-01-16 15:27:31 +0600

    Add files via upload

--- image-downloader-from-url.py
+++ image-downloader-from-url.py
@@ -1,3 +1,4 @@
 import urllib.request
 
-urllib.request.urlretrieve("http://www.gunnerkrigg.com//comics/00000001.jpg", "00000001.jpg")
+#urllib.request.urlretrieve("http://www.gunnerkrigg.com//comics/00000001.jpg", "00000001.jpg")
+urllib.request.urlretrieve("https://firebasestorage.googleapis.com/v0/b/bkashbiponon.appspot.com/o/outletimages%2F%2B8801713254678%2B8801975756699_JPEG_20200116_120109_36156792503320574286603.jpg?alt=media&token=c032764e-671c-4d6d-a7aa-7fb76018e376", "00000001.jpg")
