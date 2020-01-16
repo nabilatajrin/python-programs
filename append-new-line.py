7cf1dd483cf2ede1c0b6500d70e6183e30aec08d
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2020-01-16 11:36:37 +0600

    Add files via upload

---
+++ append-new-line.py
@@ -0,0 +1,14 @@
+x = 3
+
+print(x)          # Trailing comma suppresses newline in Python 2
+print(x, end=" ")  # Appends a space instead of a newline in Python 3
+print(x, end=" ")  # Appends a space instead of a newline in Python 3
+
+
+y = 5,              #',' keeps () in next prints
+
+print(y)          # Trailing comma suppresses newline in Python 2
+print(y, end=" ")  # Appends a space instead of a newline in Python 3
+print(y, end=" ")  # Appends a space instead of a newline in Python 3
+
+
