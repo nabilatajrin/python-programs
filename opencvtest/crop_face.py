6dff424611348e62663585b40f02c3e1a8388d0a
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2021-01-25 10:49:14 +0600

    Update crop_face.py

--- opencvtest/crop_face.py
+++ opencvtest/crop_face.py
@@ -4,34 +4,28 @@ import os
 
 class FaceCropper(object):
     CASCADE_PATH = "/media/iit/R a i n/2019/ML/pythonprojects/3b.ppm"
-
     def __init__(self):
         self.face_cascade = cv2.CascadeClassifier(self.CASCADE_PATH)
-
     def generate(self, image_path, show_result):
         img = cv2.imread("/media/iit/R a i n/2019/ML/pythonprojects/3b.ppm")
         if (img is None):
             print("Can't open image file")
             return 0
-
         #img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         faces = self.face_cascade.detectMultiScale(img, 1.1, 3, minSize=(100, 100))
         if (faces is None):
             print('Failed to detect face')
             return 0
-
         if (show_result):
             for (x, y, w, h) in faces:
                 cv2.rectangle(img, (x,y), (x+w, y+h), (255,0,0), 2)
             cv2.imshow('img', img)
             cv2.waitKey(0)
             cv2.destroyAllWindows()
-
         facecnt = len(faces)
         print("Detected faces: %d" % facecnt)
         i = 0
         height, width = img.shape[:2]
-
         for (x, y, w, h) in faces:
             r = max(w, h) / 2
             centerx = x + w / 2
@@ -39,19 +33,15 @@ class FaceCropper(object):
             nx = int(centerx - r)
             ny = int(centery - r)
             nr = int(r * 2)
-
             faceimg = img[ny:ny+nr, nx:nx+nr]
             lastimg = cv2.resize(faceimg, (32, 32))
             i += 1
             cv2.imwrite("image%d.jpg" % i, lastimg)
-
 if __name__ == '__main__':
     args = sys.argv
     argc = len(args)
-
     if (argc != 2):
         print('Usage: %s [image file]' % args[0])
         quit()
-
     detecter = FaceCropper()
     detecter.generate(args[1], True)
