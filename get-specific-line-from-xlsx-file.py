7efeca3ec9a4a8e34a8bc59d075a9e1a8a93306a
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2021-02-03 15:19:03 +0600

    Update get-specific-line-from-xlsx-file.py

--- get-specific-line-from-xlsx-file.py
+++ get-specific-line-from-xlsx-file.py
@@ -3,9 +3,7 @@ import xlrd
 
 example_url = 'http://www.excel-easy.com/examples/excel-files/fibonacci-sequence.xlsx'
 r = requests.get(example_url)  # make an HTTP request
-
 workbook = xlrd.open_workbook(file_contents=r.content)  # open workbook
 worksheet = workbook.sheet_by_index(0)  # get first sheet
 specific_row = worksheet.row(12)  # you can iterate over rows of a worksheet as well
-
 print(specific_row)  # list of cells
