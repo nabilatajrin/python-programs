e983b2100af028c49817cd456bbca3925993f503
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2021-02-03 15:18:49 +0600

    Update get-request-3.py

--- get-request-3.py
+++ get-request-3.py
@@ -1,8 +1,7 @@
 import urllib3
-http = urllib3.PoolManager()
 
+http = urllib3.PoolManager()
 resp = http.request('GET', 'http://34.94.72.188/v1/models/bkash_banner:predict')
 print(resp.data)
-
 # get the status of the response
 print(resp.status)
