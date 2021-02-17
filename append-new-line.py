4b294098240e3fef0b4f5fa68ca92161cc12c9bb
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2021-02-17 11:57:07 +0600

    Update append-new-line.py

--- append-new-line.py
+++ append-new-line.py
@@ -1,12 +1,9 @@
 x = 3
-
 print(x)          # Trailing comma suppresses newline in Python 2
 print(x, end=" ")  # Appends a space instead of a newline in Python 3
 print(x, end=" ")  # Appends a space instead of a newline in Python 3
 
-
 y = 5,              #',' keeps () in next prints
-
 print(y)          # Trailing comma suppresses newline in Python 2
 print(y, end=" ")  # Appends a space instead of a newline in Python 3
 print(y, end=" ")  # Appends a space instead of a newline in Python 3
