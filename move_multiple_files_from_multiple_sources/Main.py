0c0cae991c133e45691160481c68ea1bfda728be
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2021-02-17 11:56:17 +0600

    Update Main.py

--- move_multiple_files_from_multiple_sources/Main.py
+++ move_multiple_files_from_multiple_sources/Main.py
@@ -2,19 +2,15 @@ import os
 
 # Function to move files
 def main():
-
     src = '/media/iit/R a i n/2019/Intelligent Machines Ltd/face-propagation/dummy-data-set/colorferet/colorferet/dvd2/smaller'
     dst = '/media/iit/R a i n/2019/Intelligent Machines Ltd/face-propagation/dummy-data-set/colorferet/colorferet/dvd1/data/images/dstFolder_01/'
 
     for filename in os.listdir(src):
         print(filename)
-
         for filename2 in os.listdir(src + '/' + filename):
             print('filename2:' + filename2)
-
             srcFolder = src + '/' + filename + '/' + filename2
             dstFolder = dst + filename2
-
             # move all the files
             os.rename(srcFolder, dstFolder)
 
