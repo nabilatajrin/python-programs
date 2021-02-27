1b58c66ca2d8122b95c3e35b39fe25151bf01c84
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2021-02-27 19:58:35 +0600

    Update Test.py

--- image_to_image/Test.py
+++ image_to_image/Test.py
@@ -3,7 +3,6 @@ import numpy as np
 
 img = cv2.imread('/media/iit/R a i n/2019/Intelligent Machines Ltd/face-propagation/dummy-data-set/colorferet/colorferet/test_blur/src_blur/00002_940128_fa.ppm')
 cv2.imshow('Original', img)
-
 size = 15
 
 # generating the kernel
@@ -13,10 +12,7 @@ kernel_motion_blur = kernel_motion_blur / size
 
 # applying the kernel to the input image
 output = cv2.filter2D(img, -1, kernel_motion_blur)
-
 cv2.imshow('Motion Blur', output)
 cv2.waitKey(0)
-
 dstPath = "/media/iit/R a i n/2019/Intelligent Machines Ltd/face-propagation/dummy-data-set/colorferet/colorferet/test_blur/motion_blur/"
-
 cv2.output.save(dstPath + '.jpg', 'JPEG', quality = 90)
