831512963613a25f93d346e17598d99ebadb6513
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2021-03-01 12:37:38 +0600

    Update get-request-2.py

--- get-request-2.py
+++ get-request-2.py
@@ -1,6 +1,6 @@
 import urllib3
-http = urllib3.PoolManager()
 
+http = urllib3.PoolManager()
 resp = http.request('GET', 'http://tutorialspoint.com/robots.txt')
 print(resp.data)
 # get the status of the response
