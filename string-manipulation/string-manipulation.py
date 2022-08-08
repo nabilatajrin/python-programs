b0548a23b4ccf6700a0fc8339f8260036768b487
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2022-08-08 11:48:56 +0600

    Create string-manipulation.py

---
+++ string-manipulation/string-manipulation.py
@@ -0,0 +1,56 @@
+#new line using back slash and n
+x = 'hello \nworld'
+print(x)
+
+#print double quotes using escape character
+y = 'hello \"world\"'
+print(y)
+
+#print double slash
+a = 'hello \\ world'
+print(a)
+b = 'hello \\\ world'
+print(b)
+
+#print a tab's width
+c = 'hello \t world'
+print(c)
+
+#remove white space, \b is the backspace
+d = 'hello \bworld'
+print(d)
+
+#create a multi-line string
+e = '''this
+is
+a
+string'''
+print(e)
+
+#array of bytes
+f = 'hello'
+print(f[1])
+#print multiple chars
+print(f[0:1])
+print(f[1:3])
+print(f[1:])
+print(f[2:])
+print(f[-2:])
+
+#length of the string
+print(len(f))
+
+text = 'hello, I am 10 years old today'
+print('capitalize: ', text.capitalize())
+print('upper: ', text.upper())
+print('lower: ', text.lower())
+print('title: ', text.title())
+
+
+
+
+
+
+
+
+
