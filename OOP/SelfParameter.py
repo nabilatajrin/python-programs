09d3e62c00711e4afc81ed072afa9ed87f5e7382
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2022-08-15 21:01:44 +0600

    Update SelfParameter.py

--- OOP/SelfParameter.py
+++ OOP/SelfParameter.py
@@ -1,10 +1,10 @@
 class SelfParameter:
-  def __init__(mysillyobject, name, age):
-    mysillyobject.name = name
-    mysillyobject.age = age
+    def __init__(mysillyobject, name, age):
+        mysillyobject.name = name
+        mysillyobject.age = age
 
-  def myfunc(self):
-    print("Hello my name is " + self.name)
+    def myfunc(abc):
+        print("Hello my name is " + abc.name)
 
 p1 = SelfParameter("John", 36)
 p1.myfunc()
