6360d95a66bb4542415fea7a5af217e7e2db2831
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2022-08-15 18:43:59 +0600

    Create Person.py

---
+++ OOP/Person.py
@@ -0,0 +1,15 @@
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def myfunc(self):
+    print("Hello my name is " + self.name)
+
+p1 = Person("John", 36)
+
+print(p1.name)
+print(p1.age)
+print(p1.name, p1.age)
+
+p1.myfunc()
