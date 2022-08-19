17ab9786054f50f77ee0b519ad34668661a06733
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2022-08-19 18:22:30 +0600

    Create output-var.py

---
+++ var/output-var.py
@@ -0,0 +1,18 @@
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+
+x = "Python "
+y = "is "
+z = "awesome"
+print(x + y + z)
+
+x = 5
+y = "John"
+#print(x + y) #err, can't concatenate
+
+x = 5
+y = "John"
+print(x, y)
+
