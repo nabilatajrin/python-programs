d30942fd46118906cb5dbea6a832e9f45b4dbe84
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2022-08-20 14:18:00 +0600

    Create check-string.py

---
+++ string-manipulation/check-string.py
@@ -0,0 +1,19 @@
+#String Length
+a = "Hello, World!"
+print(len(a))
+
+#Check String
+txt = "The best things in life are free!"
+print("free" in txt)
+
+txt = "The best things in life are free!"
+if "free" in txt:
+  print("Yes, 'free' is present.")
+
+#Check if NOT
+txt = "The best things in life are free!"
+print("expensive" not in txt)
+
+txt = "The best things in life are free!"
+if "expensive" not in txt:
+  print("No, 'expensive' is NOT present.")
