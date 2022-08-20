06015aafb09f8cf7b021e0af94e309fd3403c6ea
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2022-08-20 14:13:58 +0600

    Create string-array.py

---
+++ string-manipulation/string-array.py
@@ -0,0 +1,8 @@
+"""
+Like many other popular programming languages, strings in Python are arrays of bytes representing unicode characters.
+However, Python does not have a character data type, a single character is simply a string with a length of 1.
+Square brackets can be used to access elements of the string.
+"""
+
+a = "Hello, World!"
+print(a[1])
