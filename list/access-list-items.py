e9a6bef4b6aad010995f55b28ce2f3f2759009ce
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2022-08-21 16:46:28 +0600

    Create access-list-items.py

---
+++ list/access-list-items.py
@@ -0,0 +1,26 @@
+#Access Items
+thislist = ["apple", "banana", "cherry"]
+print(thislist[1])
+
+#Negative Indexing
+thislist = ["apple", "banana", "cherry"]
+print(thislist[-1])
+
+#Range of Indexes
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:5])
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[:4])
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:])
+
+#Range of Negative Indexes
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[-4:-1])
+
+#Check if Item Exists
+thislist = ["apple", "banana", "cherry"]
+if "apple" in thislist:
+  print("Yes, 'apple' is in the fruits list")
