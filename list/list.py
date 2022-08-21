47d661fa24a8bea279170b8fa813fc2feea2a279
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2022-08-21 16:21:35 +0600

    Create list.py

---
+++ list/list.py
@@ -0,0 +1,22 @@
+mylist = ["apple", "banana", "cherry"]
+#List
+thislist = ["apple", "banana", "cherry"]
+print(thislist)
+#Allow Duplicates
+thislist = ["apple", "banana", "cherry", "apple", "cherry"]
+print(thislist)
+#List Length
+thislist = ["apple", "banana", "cherry"]
+print(len(thislist))
+#List Items - Data Types
+list1 = ["apple", "banana", "cherry"]
+list2 = [1, 5, 7, 9, 3]
+list3 = [True, False, False]
+
+list1 = ["abc", 34, True, 40, "male"]
+#type()
+mylist = ["apple", "banana", "cherry"]
+print(type(mylist))
+#The list() Constructor
+thislist = list(("apple", "banana", "cherry")) # note the double round-brackets
+print(thislist)
