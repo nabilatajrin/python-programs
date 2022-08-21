b18432b92dad75343039d0ba1a72f84f84451b4e
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2022-08-21 19:52:15 +0600

    Create add-list-items.py

---
+++ list/add-list-items.py
@@ -0,0 +1,21 @@
+#Append Items
+thislist = ["apple", "banana", "cherry"]
+thislist.append("orange")
+print(thislist)
+
+#Insert Items
+thislist = ["apple", "banana", "cherry"]
+thislist.insert(1, "orange")
+print(thislist)
+
+#Extend List
+thislist = ["apple", "banana", "cherry"]
+tropical = ["mango", "pineapple", "papaya"]
+thislist.extend(tropical)
+print(thislist)
+
+#Add Any Iterable
+thislist = ["apple", "banana", "cherry"]
+thistuple = ("kiwi", "orange")
+thislist.extend(thistuple)
+print(thislist)
