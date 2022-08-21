2b38d9c2866c18de5da3ee23fec05dad7363fed1
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2022-08-21 17:47:43 +0600

    Update change-list-items.py

--- list/change-list-items.py
+++ list/change-list-items.py
@@ -7,11 +7,13 @@ print(thislist)
 thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
 thislist[1:3] = ["blackcurrant", "watermelon"]
 print(thislist)
-
+#If you insert more items than you replace, the new items will be inserted where you specified, and the remaining 
+# items will move accordingly:
 thislist = ["apple", "banana", "cherry"]
 thislist[1:2] = ["blackcurrant", "watermelon"]
 print(thislist)
-
+#If you insert less items than you replace, the new items will be inserted where you specified, and the remaining 
+# items will move accordingly:
 thislist = ["apple", "banana", "cherry"]
 thislist[1:3] = ["watermelon"]
 print(thislist)
