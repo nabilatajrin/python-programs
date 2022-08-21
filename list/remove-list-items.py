bb7c8ac44c5b5354ea9cfbd9594eecd4a2fab136
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2022-08-21 20:49:47 +0600

    Create remove-list-items.py

---
+++ list/remove-list-items.py
@@ -0,0 +1,25 @@
+#Remove Specified Item
+thislist = ["apple", "banana", "cherry"]
+thislist.remove("banana")
+print(thislist)
+
+#Remove Specified Index
+thislist = ["apple", "banana", "cherry"]
+thislist.pop(1)
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+thislist.pop()
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+del thislist[0]
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+del thislist
+
+#Clear the List
+thislist = ["apple", "banana", "cherry"]
+thislist.clear()
+print(thislist)
