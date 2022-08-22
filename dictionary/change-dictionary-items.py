237906798431600ff3d12283824a789b2273c76e
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2022-08-22 23:15:11 +0600

    Create change-dictionary-items.py

---
+++ dictionary/change-dictionary-items.py
@@ -0,0 +1,15 @@
+#Change Values
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict["year"] = 2018
+
+#Update Dictionary
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.update({"year": 2020})
