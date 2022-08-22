be9a68b11c9fdaa2d7154c9c702f18ffd29ceaec
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2022-08-22 17:06:25 +0600

    Create set-basics.py

---
+++ set/set-basics.py
@@ -0,0 +1,26 @@
+thisset = {"apple", "banana", "cherry"}
+print(thisset)
+
+#Duplicates Not Allowed
+thisset = {"apple", "banana", "cherry", "apple"}
+print(thisset)
+
+#Get the Length of a Set
+thisset = {"apple", "banana", "cherry"}
+print(len(thisset))
+
+#Set Items - Data Types
+set1 = {"apple", "banana", "cherry"}
+set2 = {1, 5, 7, 9, 3}
+set3 = {True, False, False}
+
+set1 = {"abc", 34, True, 40, "male"}
+
+#type()
+myset = {"apple", "banana", "cherry"}
+print(type(myset))
+
+#The set() Constructor
+thisset = set(("apple", "banana", "cherry")) # note the double round-brackets
+print(thisset)
+
