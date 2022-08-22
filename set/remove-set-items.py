6bdfaf1f94f7377ef0a4de1390ed6e4bb7837f44
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2022-08-22 17:15:37 +0600

    Create remove-set-items.py

---
+++ set/remove-set-items.py
@@ -0,0 +1,21 @@
+#Remove Item
+thisset = {"apple", "banana", "cherry"}
+thisset.remove("banana")
+print(thisset)
+
+thisset = {"apple", "banana", "cherry"}
+thisset.discard("banana")
+print(thisset)
+
+thisset = {"apple", "banana", "cherry"}
+x = thisset.pop()
+print(x)
+print(thisset)
+
+thisset = {"apple", "banana", "cherry"}
+thisset.clear()
+print(thisset)
+
+thisset = {"apple", "banana", "cherry"}
+del thisset
+#print(thisset) #err: doesn't exist
