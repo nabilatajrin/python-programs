8ac64da88509b25b20f32bcf2e0029a188b25711
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2022-08-22 16:55:05 +0600

    Create loop-tuples.py

---
+++ tuple/loop-tuples.py
@@ -0,0 +1,16 @@
+#Loop Through a Tuple
+thistuple = ("apple", "banana", "cherry")
+for x in thistuple:
+  print(x)
+
+#Loop Through the Index Numbers
+thistuple = ("apple", "banana", "cherry")
+for i in range(len(thistuple)):
+  print(thistuple[i])
+
+#Using a While Loop
+thistuple = ("apple", "banana", "cherry")
+i = 0
+while i < len(thistuple):
+  print(thistuple[i])
+  i = i + 1
