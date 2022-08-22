a1e09903b95eb810ccfef77b23d27d63b5bd70f3
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2022-08-22 16:47:07 +0600

    Create update-tuples.py

---
+++ tuple/update-tuples.py
@@ -0,0 +1,28 @@
+#Change Tuple Values
+x = ("apple", "banana", "cherry")
+y = list(x)
+y[1] = "kiwi"
+x = tuple(y)
+
+print(x)
+
+#Add Items
+thistuple = ("apple", "banana", "cherry")
+y = list(thistuple)
+y.append("orange")
+thistuple = tuple(y)
+
+thistuple = ("apple", "banana", "cherry")
+y = ("orange",)
+thistuple += y
+print(thistuple)
+
+#Remove Items
+thistuple = ("apple", "banana", "cherry")
+y = list(thistuple)
+y.remove("apple")
+thistuple = tuple(y)
+
+thistuple = ("apple", "banana", "cherry")
+del thistuple
+print(thistuple) #this will raise an error because the tuple no longer exists
