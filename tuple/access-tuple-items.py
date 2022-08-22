e056de3432a01a5b4cab85b29014cd62119683f0
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2022-08-22 16:35:50 +0600

    Create access-tuple-items.py

---
+++ tuple/access-tuple-items.py
@@ -0,0 +1,26 @@
+#Access Tuple Items
+thistuple = ("apple", "banana", "cherry")
+print(thistuple[1])
+
+#Negative Indexing
+thistuple = ("apple", "banana", "cherry")
+print(thistuple[-1])
+
+#Range of Indexes
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[2:5])
+
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[:4])
+
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[2:])
+
+#Range of Negative Indexes
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[-4:-1])
+
+#Check if Item Exists
+thistuple = ("apple", "banana", "cherry")
+if "apple" in thistuple:
+  print("Yes, 'apple' is in the fruits tuple")
