1c451efa79cba416cfc15cf8ba38ffa6496a76b4
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2022-08-23 21:03:31 +0600

    Create remove-dictionary-items.py

---
+++ dictionary/remove-dictionary-items.py
@@ -0,0 +1,40 @@
+#Removing Items
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.pop("model")
+print(thisdict)
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.popitem()
+print(thisdict)
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+del thisdict["model"]
+print(thisdict)
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+#del thisdict
+print(thisdict) #this will cause an error because "thisdict" no longer exists.
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.clear()
+print(thisdict)
