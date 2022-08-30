9aa55c713f4217fee6bd030b24a8f0378d272bfc
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2022-08-30 17:35:22 +0600

    Create iterator.py

---
+++ loop/iterator.py
@@ -0,0 +1,65 @@
+mytuple = ("apple", "banana", "cherry")
+myit = iter(mytuple)
+
+print(next(myit))
+print(next(myit))
+
+mystr = "banana"
+myit = iter(mystr)
+
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))
+#print(next(myit)) #err when extra iter
+
+#Looping Through an Iterator
+mytuple = ("apple", "banana", "cherry")
+for x in mytuple:
+  print(x)
+
+mystr = "banana"
+for x in mystr:
+  print(x)
+
+#Create an Iterator
+class MyNumbers:
+    def __iter__(self):
+        self.a = 1
+        return self
+
+    def __next__(self):
+        x = self.a
+        self.a += 1
+        return x
+
+myclass = MyNumbers()
+myiter = iter(myclass)
+
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+
+#StopIteration
+class MyNumbers:
+  def __iter__(self):
+    self.a = 1
+    return self
+
+  def __next__(self):
+    if self.a <= 20:
+      x = self.a
+      self.a += 1
+      return x
+    else:
+      raise StopIteration
+
+myclass = MyNumbers()
+myiter = iter(myclass)
+
+for x in myiter:
+  print(x)
