d971c0baafe1bd7e23f894ace2e82108a14774a3
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2022-08-30 16:51:23 +0600

    Create if-else.py

---
+++ if-else/if-else.py
@@ -0,0 +1,31 @@
+a = 33
+b = 200
+if b > a:
+  print("b is greater than a")
+
+#elif
+a = 33
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+
+#else
+a = 200
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+else:
+  print("a is greater than b")
+
+a = 200
+b = 33
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+
+  
