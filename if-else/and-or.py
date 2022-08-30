25e86174730d49f2fb9b9f1a6e905fddcbb78c7b
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2022-08-30 16:51:02 +0600

    Create and-or.py

---
+++ if-else/and-or.py
@@ -0,0 +1,12 @@
+a = 200
+b = 33
+c = 500
+if a > b and c > a:
+  print("Both conditions are True")
+
+a = 200
+b = 33
+c = 500
+if a > b or a > c:
+  print("At least one of the conditions is True")
+
