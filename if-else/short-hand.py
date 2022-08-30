a53d6b274ae1b3b9e24539dde1e9a95f3b30658c
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2022-08-30 16:52:47 +0600

    Create short-hand.py

---
+++ if-else/short-hand.py
@@ -0,0 +1,13 @@
+#Short Hand If
+a = 2
+b = 330
+if a > b: print("a is greater than b")
+
+#Short Hand If ... Else
+a = 2
+b = 330
+print("A") if a > b else print("B")
+
+a = 330
+b = 330
+print("A") if a > b else print("=") if a == b else print("B")
