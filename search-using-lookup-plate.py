10849517fa734baecb4c326a568322f9a9289c86
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2022-12-17 18:31:13 +0600

    Update search-using-lookup-plate.py

--- search-using-lookup-plate.py
+++ search-using-lookup-plate.py
@@ -2,3 +2,14 @@
 # Use * to represent the missing four letters
 plate = 'FRQ****'
 FRQ_plate = lookup_plate(plate)
+
+
+# Define plate to represent a plate beginning with FRQ
+# Use * to represent the missing four letters
+plate = 'FRQ****'
+
+# Call the function lookup_plate()
+lookup_plate(plate)
+
+# Call lookup_plate() with the keyword argument for color
+lookup_plate(plate, color='blue')
