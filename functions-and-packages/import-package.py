b0665a4ea34a2eb46a24ef2f5c61bd5359eedb08
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2023-11-03 15:33:58 +0600

    Create import-package.py

---
+++ functions-and-packages/import-package.py
@@ -0,0 +1,15 @@
+# Import the math package
+import math
+
+# Definition of radius
+r = 0.43
+
+# Calculate C
+C = 2 * math.pi * r
+
+# Calculate A
+A = math.pi * r ** 2
+
+# Build printout
+print("Circumference: " + str(C))
+print("Area: " + str(A))
