42db14204ca7f73fda34c4d7baf6c080484ecb4f
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2023-11-03 15:57:00 +0600

    Create numpy-array.py

---
+++ NumPy/numpy-array.py
@@ -0,0 +1,11 @@
+# Import the numpy package as np
+import numpy as np
+
+# Create list baseball
+baseball = [180, 215, 210, 210, 188, 176, 209, 200]
+
+# Create a numpy array from baseball: np_baseball
+np_baseball = np.array(baseball)
+
+# Print out type of np_baseball
+print(np_baseball)
