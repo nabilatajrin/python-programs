c3269e1bd48ec698b2e2009e293a82fc0006e91c
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2023-12-03 12:30:39 +0600

    Create line-plot-(1).py

---
+++ Matplotlib/line-plot-(1).py
@@ -0,0 +1,11 @@
+# Print the last item from year and pop
+print(year, pop)
+
+# Import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+
+# Make a line plot: year on the x-axis, pop on the y-axis
+plt.plot(year, pop)
+
+# Display the plot with plt.show()
+plt.show()
