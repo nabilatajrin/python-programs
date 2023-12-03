5190a377ef09098fa1cdba6bd5a2070fd18a30be
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2023-12-03 12:41:46 +0600

    Create line-plot-02.py

---
+++ Matplotlib/line-plot-02.py
@@ -0,0 +1,9 @@
+# Print the last item of gdp_cap and life_exp
+print(gdp_cap, life_exp)
+
+# Make a line plot, gdp_cap on the x-axis, life_exp on the y-axis
+import matplotlib.pyplot as plt
+plt.plot(gdp_cap, life_exp)
+
+# Display the plot
+plt.show()
