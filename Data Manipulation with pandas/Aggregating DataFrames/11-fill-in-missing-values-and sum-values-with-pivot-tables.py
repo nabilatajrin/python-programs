368df31b246e5058b7dfa0d32543bf3b8e2f343f
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2023-12-15 20:12:30 +0600

    Create 11-fill-in-missing-values-and sum-values-with-pivot-tables.py

---
+++ Data Manipulation with pandas/Aggregating DataFrames/11-fill-in-missing-values-and sum-values-with-pivot-tables.py
@@ -0,0 +1,2 @@
+# Print the mean weekly_sales by department and type; fill missing values with 0s; sum all rows and cols
+print(sales.pivot_table(values="weekly_sales", index="department", columns="type", fill_value=0, margins=True))
